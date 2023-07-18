67a4dbfab5551441310329660f4480e1e695312f
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-07-17 20:11:32 -0700

    Day 6: more states (ex: Green: forward then left; left = spin until color))

--- Week2/main.py
+++ Week2/main.py
@@ -29,9 +29,9 @@ GPIO.setup(GPIO_Bin1, GPIO.OUT)
 GPIO.setup(GPIO_Bin2, GPIO.OUT)
 GPIO.setup(GPIO_Bpwm, GPIO.OUT)
 
-GPIO.output(GPIO_Ain1, False)
+GPIO.output(GPIO_Ain1, True)
 GPIO.output(GPIO_Ain2, False)
-GPIO.output(GPIO_Bin1, False)
+GPIO.output(GPIO_Bin1, True)
 GPIO.output(GPIO_Bin2, False)
 
 pwm_frequency = 50
@@ -44,9 +44,7 @@ pwmB.start(0)
 
 left_speed = 0.0
 right_speed = 0.0
-# ------------------------------------------------------------------------------#
-
-
+#------------------------------------------------------------------------------#
 COLOR_HUE_RANGES = {
     "GREEN": [60, 80],
     "PINK": [150, 180],  # PINK + ORANGE
@@ -56,6 +54,7 @@ COLOR_HUE_RANGES = {
     "NAVY": [100, 120],  # NOT GREAT FIT?
 }
 
+# TODO: make specific per color
 SAT_RANGES = [60, 250]
 VAL_RANGES = [60, 250]
 
@@ -75,7 +74,6 @@ camera.hflip = False
 # Create an array to store a frame
 rawframe = picamera.array.PiRGBArray(camera, size=(SCREEN_SIZE[0], SCREEN_SIZE[1]))
 #------------------------------------------------------------------------------#
-
 """
 State: start
     - Wait until a color is detected
@@ -131,6 +129,33 @@ class FSM:
             return "RIGHT"
 
 currentState = FSM.START
+#------------------------------------------------------------------------------#
+def mask_image(image, image_hsv, color):
+    # Note: color is like ["GREEN"]
+    lowerThreshold = np.array([COLOR_HUE_RANGES[color][0], SAT_RANGES[0], VAL_RANGES[0]])
+    upperThreshold = np.array([COLOR_HUE_RANGES[color][1], SAT_RANGES[1], VAL_RANGES[1]])
+
+    color_mask = cv2.inRange(image_hsv, lowerThreshold, upperThreshold)
+    image_masked = cv2.bitwise_and(image, image, mask=color_mask)
+
+    return color_mask, image_masked
+
+def center_of_mask(color_mask):
+    cnts = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    cx = None
+    cy = None
+
+    for c in cnts[0]:
+        M = cv2.moments(c)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            
+            # Is break necessary?
+            break
+    
+    return cx, cy, cnts
 
 
 try:
@@ -146,62 +171,57 @@ try:
         print("Current state: " + FSM.to_string(currentState))
 
 
-        if currentState == FSM.START:
+        if currentState == FSM.START or currentState == FSM.LEFT or currentState == FSM.RIGHT:
+            if currentState == FSM.LEFT:
+                left_speed = 0.0
+                right_speed = 30.0
+            elif currentState == FSM.RIGHT:
+                left_speed = 30.0
+                right_speed = 0.0
+            elif currentState == FSM.START:
+                left_speed = 0.0
+                right_speed = 0.0
+
             # for color in COLOR_HUE_RANGES:
             for color in ["GREEN"]:
-                lowerThreshold = np.array([COLOR_HUE_RANGES[color][0], SAT_RANGES[0], VAL_RANGES[0]])
-                upperThreshold = np.array([COLOR_HUE_RANGES[color][1], SAT_RANGES[1], VAL_RANGES[1]])
-
-                color_mask = cv2.inRange(image_hsv, lowerThreshold, upperThreshold)
-
+                color_mask, _ = mask_image(image, image_hsv, color)
                 cnts = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
                 if len(cnts[0]) > 0:
-                    # not sure if this will work
                     print("Found color: " + color)
                     currentState = FSM.color_to_state(color)
                     break
-        elif currentState == FSM.GREEN:
+        elif currentState == FSM.GREEN or currentState == FSM.PINK:
             # cv2.destroyWindow(winname)
-            lowerThreshold = np.array([COLOR_HUE_RANGES["GREEN"][0], SAT_RANGES[0], VAL_RANGES[0]])
-            upperThreshold = np.array([COLOR_HUE_RANGES["GREEN"][1], SAT_RANGES[1], VAL_RANGES[1]])
+            color = FSM.state_to_color(currentState)
+            color_mask, image_masked = mask_image(image, image_hsv, color)
 
-            green_mask = cv2.inRange(image_hsv, lowerThreshold, upperThreshold)
-            image_masked = cv2.bitwise_and(image, image, mask=green_mask)
-
-            cnts = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cx, cy, cnts = center_of_mask(color_mask)
 
             cv2.drawContours(image, cnts[0], -1, (255, 0, 0), 2)
             cv2.drawContours(image_masked, cnts[0], -1, (255, 0, 0), 2)
 
-            cx = None
-            cy = None
-
-            for i, c in enumerate(cnts[0]):
-                M = cv2.moments(c)
-                if M["m00"] != 0:
-                    cx = int(M["m10"] / M["m00"])
-                    cy = int(M["m01"] / M["m00"])
-
-                    cv2.circle(image, (cx, cy), 5, (255, 255, 255), -1)
-                    cv2.circle(image_masked, (cx, cy), 5, (255, 255, 255), -1)
-                    break
+            if cx is not None and cy is not None:
+                cv2.circle(image, (cx, cy), 5, (255, 255, 255), -1)
+                cv2.circle(image_masked, (cx, cy), 5, (255, 255, 255), -1)
+                    
 
             cutoff = SCREEN_SIZE[1] * BOTTOM_CUTOFF
             if cy is not None and cy < cutoff:
-                currentState = FSM.LEFT
+                if currentState == FSM.GREEN:
+                    currentState = FSM.LEFT
+                elif currentState == FSM.PINK:
+                    currentState = FSM.RIGHT
             elif cx is not None and cy is not None:
                 left_spacing = cx / SCREEN_SIZE[0]
                 left_speed = left_spacing * 100.0
                 right_speed = (1 - left_spacing) * 100.0
+
+                cv2.circle(image, (cx, cy), 5, (255, 255, 255), -1)
+                cv2.circle(image_masked, (cx, cy), 5, (255, 255, 255), -1)
             # else:
             #     left_speed = 0
             #     right_speed = 0
-
-            GPIO.output(GPIO_Ain1, True)
-            GPIO.output(GPIO_Ain2, False)
-            GPIO.output(GPIO_Bin1, True)
-            GPIO.output(GPIO_Bin2, False)
 
 
             cv2.imshow("Masked image", image_masked)
@@ -212,10 +232,9 @@ try:
             cv2.line(image, start_point, end_point, (0, 255, 0), 2)
             cv2.line(image_masked, start_point, end_point, (0, 255, 0), 2)
 
-            # pwmB.ChangeDutyCycle(left_speed)
-            # pwmA.ChangeDutyCycle(right_speed)
 
-
+        # pwmB.ChangeDutyCycle(left_speed)
+        # pwmA.ChangeDutyCycle(right_speed)
 
         cv2.imshow("Frame in BGR", image)
         # cv2.imshow("Frame in HSV", image_hsv)
