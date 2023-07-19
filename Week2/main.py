73464e5de1746bf0125054b2f57c0488bacaa9e0
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-07-18 18:05:34 -0700

    Day 7: readability/organization improvements + yellow tracks as long as possible

--- Week2/main.py
+++ Week2/main.py
@@ -1,7 +1,3 @@
-# This program illustrates how to capture frames in a video stream and
-# and how to do extract pixels of a specific color
-# It uses openCV
-
 import picamera
 import picamera.array  # This needs to be imported explicitly
 import cv2
@@ -13,9 +9,11 @@ import time
 
 """
 TODO:
-Possible ideas:
-- Calibrate bottom cutoff
-- Calibrate color ranges
+- Calibrate bottom cutoff?
+- Calibrate/improve color thresholds?
+- Try to improve FPS
+- Tweak speeds
+- Non-linear vertical modifier?
 """
 
 # ------------------------------------------------------------------------------#
@@ -53,29 +51,24 @@ right_speed = 0.0
 #------------------------------------------------------------------------------#
 COLOR_HUE_RANGES = {
     "GREEN": {
-        "H": [40, 80],
-        "S": [75, 255],
-        "V": [75, 255],
+        "LOWER": [40, 75, 75],
+        "UPPER": [80, 255, 255],
     },
     "PINK": {
-        "H": [150, 180],
-        "S": [75, 255],
-        "V": [75, 255],
+        "LOWER": [150, 75, 75],
+        "UPPER": [180, 255, 255],
     },
     "YELLOW": {
-        "H": [25, 50],
-        "S": [70, 255],
-        "V": [75, 255],
+        "LOWER": [25, 70, 75],
+        "UPPER": [50, 255, 255],
     },
     "PURPLE": {
-        "H": [135, 160],
-        "S": [140, 255],
-        "V": [10, 255],
+        "LOWER": [135, 140, 10],
+        "UPPER": [160, 255, 255],
     },
     "BLUE": {
-        "H": [80, 110],
-        "S": [140, 255],
-        "V": [10, 255],
+        "LOWER": [80, 140, 10],
+        "UPPER": [140, 255, 255],
     },
     # "NAVY": [100, 120],  # NOT GREAT FIT?
 }
@@ -88,7 +81,7 @@ SCREEN_SIZE = [480, 368]
 
 BOTTOM_CUTOFF = 0.8
 
-""
+DISPLAY_DEBUG = True
 
 
 # Initialize the camera and grab a reference to the frame
@@ -106,14 +99,15 @@ rawframe = picamera.array.PiRGBArray(camera, size=(SCREEN_SIZE[0], SCREEN_SIZE[1
 State: start
     - Wait until a color is detected
 - State: green
-    - While green is detected & the center is above the cutoff line: go forward
-    - Then turn left until another tag is detected
+    - Track to green while center is above the cutoff line
+    - Then state = left
 - State: pink
-    - Same as green, but turn right
+    - Same as green, but state = right
 - State: yellow
-    - Go forward until another tag is detected
-- State: left/right
-    - Turn left/right until another tag is detected
+    - Track to yellow while center is above the cutoff line
+    - Then state = straight
+- State: left/right/straight
+    - Turn left/right/straight until another tag (above the cutoff) is detected
 """
 class FSM:
     START = 0
@@ -123,6 +117,7 @@ class FSM:
 
     LEFT = 4
     RIGHT = 5
+    STRAIGHT = 6
 
     @classmethod
     def color_to_state(cls, color):
@@ -156,31 +151,21 @@ class FSM:
             return "LEFT"
         elif state == cls.RIGHT:
             return "RIGHT"
+        elif state == cls.STRAIGHT:
+            return "STRAIGHT"
 
 currentState = FSM.START
 #------------------------------------------------------------------------------#
-def mask_image(image, image_hsv, color):
-    # Note: color is like ["GREEN"]
-    lowerThreshold = np.array([
-        COLOR_HUE_RANGES[color]["H"][0],
-        COLOR_HUE_RANGES[color]["S"][0],
-        COLOR_HUE_RANGES[color]["V"][0],
-    ])
-    upperThreshold = np.array([
-        COLOR_HUE_RANGES[color]["H"][1],
-        COLOR_HUE_RANGES[color]["S"][1],
-        COLOR_HUE_RANGES[color]["V"][1],
-    ])
+def mask_image(image_hsv, color):
+    lowerThreshold = np.array(COLOR_HUE_RANGES[color]["LOWER"])
+    upperThreshold = np.array(COLOR_HUE_RANGES[color]["UPPER"])
 
     color_mask = cv2.inRange(image_hsv, lowerThreshold, upperThreshold)
-    image_masked = cv2.bitwise_and(image, image, mask=color_mask)
 
-    # cv2.imshow("Masked image", image_masked)
-
-    return color_mask, image_masked
+    return color_mask
 
 def center_of_mask(color_mask):
-    cnts, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, _ = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     cx = None
     cy = None
@@ -194,17 +179,10 @@ def center_of_mask(color_mask):
     #         break
     # return cx, cy, cnts
 
-    boxes = []
-
-    for c in cnts:
-        rect = cv2.minAreaRect(c)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-
-        boxes.append(box)
+    boxes = (np.int0(cv2.boxPoints(cv2.minAreaRect(c))) for c in cnts)
 
     # choose the box with the largest area
-    if len(boxes) > 0:
+    try:
         max_box = max(boxes, key=cv2.contourArea)
 
         # cx and cy are averages of the corners
@@ -212,9 +190,10 @@ def center_of_mask(color_mask):
         cy = int(np.mean(max_box[:, 1]))
 
         cnts = [max_box]
+    except ValueError: # max() arg is an empty sequence
+        pass
 
     return cx, cy, cnts
-
 
 t0 = time.time()
 t1 = time.time()
@@ -222,8 +201,15 @@ delta = 1 / 32
 
 MAX_SPEED = 50.0
 SPIN_MOTOR_SPEED = 30.0
-BLIND_MOTOR_SPEED = 30.0
+STRAIGHT_MOTOR_SPEED = 30.0
 MIN_VERTICAL_MODIFIER = 0.5
+
+def vertical_modifier_linear(top_spacing):
+    global MIN_VERTICAL_MODIFIER, BOTTOM_CUTOFF
+    return (MIN_VERTICAL_MODIFIER - 1.0) / (BOTTOM_CUTOFF - 0.5) * (top_spacing - 0.5) + 1.0
+
+def vertical_modifier_exponential(top_spacing):
+    return -10.0 * (top_spacing - 0.5) ** 2 + 1.0
 
 try:
     
@@ -246,24 +232,23 @@ try:
         # print("Current state: " + FSM.to_string(currentState), left_speed, right_speed)
 
 
-        if currentState in [FSM.START, FSM.YELLOW, FSM.LEFT, FSM.RIGHT]:
+        if currentState in [FSM.START, FSM.LEFT, FSM.RIGHT, FSM.STRAIGHT]:
             if currentState == FSM.START:
                 left_speed = 0.0
                 right_speed = 0.0
-            elif currentState == FSM.YELLOW:
-                left_speed = BLIND_MOTOR_SPEED
-                right_speed = BLIND_MOTOR_SPEED
             elif currentState == FSM.LEFT:
                 left_speed = 0.0
                 right_speed = SPIN_MOTOR_SPEED
             elif currentState == FSM.RIGHT:
                 left_speed = SPIN_MOTOR_SPEED
                 right_speed = 0.0
+            elif currentState == FSM.STRAIGHT:
+                left_speed = STRAIGHT_MOTOR_SPEED
+                right_speed = STRAIGHT_MOTOR_SPEED
 
-            # TODO
             # for color in COLOR_HUE_RANGES:
-            for color in ["GREEN", "PINK"]:
-                color_mask, _ = mask_image(image, image_hsv, color)
+            for color in ["GREEN", "PINK", "YELLOW"]:
+                color_mask = mask_image(image_hsv, color)
                 cx, cy, cnts = center_of_mask(color_mask)
 
                 cutoff = SCREEN_SIZE[1] * BOTTOM_CUTOFF
@@ -271,20 +256,11 @@ try:
                     print("Found color: " + color)
                     currentState = FSM.color_to_state(color)
                     break
-        elif currentState == FSM.GREEN or currentState == FSM.PINK:
-            # cv2.destroyWindow(winname)
+        elif currentState in [FSM.GREEN, FSM.PINK, FSM.YELLOW]:
             color = FSM.state_to_color(currentState)
-            color_mask, image_masked = mask_image(image, image_hsv, color)
+            color_mask = mask_image(image_hsv, color)
 
             cx, cy, cnts = center_of_mask(color_mask)
-
-            cv2.drawContours(image, cnts, -1, (255, 0, 0), 2)
-            cv2.drawContours(image_masked, cnts, -1, (255, 0, 0), 2)
-
-            if cx is not None and cy is not None:
-                cv2.circle(image, (cx, cy), 5, (255, 255, 255), -1)
-                cv2.circle(image_masked, (cx, cy), 5, (255, 255, 255), -1)
-                    
 
             cutoff = SCREEN_SIZE[1] * BOTTOM_CUTOFF
             if cy is not None and cy > cutoff:
@@ -292,12 +268,15 @@ try:
                     currentState = FSM.LEFT
                 elif currentState == FSM.PINK:
                     currentState = FSM.RIGHT
+                elif currentState == FSM.YELLOW:
+                    currentState = FSM.STRAIGHT
             elif cx is not None and cy is not None:
                 left_spacing = cx / SCREEN_SIZE[0]
                 right_spacing = 1 - left_spacing
 
                 top_spacing = cy / SCREEN_SIZE[1]
-                vertical_modifier = (MIN_VERTICAL_MODIFIER - 1.0) / (BOTTOM_CUTOFF - 0.5) * (top_spacing - 0.5) + 1.0
+                vertical_modifier = vertical_modifier_exponential(top_spacing)
+                # vertical_modifier = vertical_modifier_linear(top_spacing)
                 vertical_modifier = min(1.0, vertical_modifier)
                 # print("Vertical modifier: %.2f" % vertical_modifier)
 
@@ -308,33 +287,31 @@ try:
                 left_speed = left_spacing * MAX_SPEED * vertical_modifier
                 right_speed = right_spacing * MAX_SPEED * vertical_modifier
 
-                cv2.circle(image, (cx, cy), 5, (255, 255, 255), -1)
-                cv2.circle(image_masked, (cx, cy), 5, (255, 255, 255), -1)
+                if DISPLAY_DEBUG:
+                    cv2.circle(image, (cx, cy), 5, (255, 255, 255), -1)
 
 
-            # cv2.imshow("Masked image", image_masked)
+            if DISPLAY_DEBUG:
+                cv2.drawContours(image, cnts, -1, (255, 0, 0), 2)
+                if cx is not None and cy is not None:
+                    cv2.circle(image, (cx, cy), 5, (255, 255, 255), -1)
 
-            start_point = (0, int(SCREEN_SIZE[1] * BOTTOM_CUTOFF))
-            end_point = (SCREEN_SIZE[0], int(SCREEN_SIZE[1] * BOTTOM_CUTOFF))
+                start_point = (0, int(SCREEN_SIZE[1] * BOTTOM_CUTOFF))
+                end_point = (SCREEN_SIZE[0], int(SCREEN_SIZE[1] * BOTTOM_CUTOFF))
 
-            cv2.line(image, start_point, end_point, (0, 255, 0), 2)
-            cv2.line(image_masked, start_point, end_point, (0, 255, 0), 2)
+                cv2.line(image, start_point, end_point, (0, 255, 0), 2)
+                # cv2.line(image_masked, start_point, end_point, (0, 255, 0), 2)
         else:
             ...
 
+        # Motors
+        # pwmB.ChangeDutyCycle(left_speed)
+        # pwmA.ChangeDutyCycle(right_speed)
 
-        pwmB.ChangeDutyCycle(left_speed)
-        pwmA.ChangeDutyCycle(right_speed)
-
-        cv2.imshow("Frame in BGR", image)
-        # cv2.imshow("Frame in HSV", image_hsv)
-        # cv2.imshow("Mask", ourmask)
-        # cv2.imshow("Masked image", image_masked)
-
+        if DISPLAY_DEBUG:
+            cv2.imshow("Frame in BGR", image)
+            cv2.waitKey(1)
         
-        cv2.waitKey(1)
-
-        # Clear the stream in preparation for the next frame
         rawframe.truncate(0)
 
 
